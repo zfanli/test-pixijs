1c5d7910fafe42fb39d5eecc0f65e74bc0eb1e9a
Richard <zfanli@outlook.com>
2019-11-03 23:55:55 +0800

    Update first try with pixijs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import * as PIXI from 'pixi.js'
-
 import './css/index.css'
 
-let type = 'WebGL'
-if (!PIXI.utils.isWebGLSupported()) {
-  type = 'canvas'
-}
+//Create a Pixi Application
+let app = new PIXI.Application({
+  // `width`: The width of the renderers view.
+  width: window.innerWidth, // default: 800
+  // `height`: The height of the renderers view.
+  height: window.innerHeight, // default: 600
+  // `antialias`: Sets antialias.
+  antialias: true, // default: false
+  // `transparent`: If the render view is transparent.
+  transparent: true, // default: false
+  // `resolution`: The resolution / device pixel ratio of the renderer,
+  // retina would be 2.
+  resolution: 2, // default: 1
+  // `autoDensity`: Resizes renderer view in CSS pixels to allow for
+  // resolutions other than 1.
+  autoDensity: true, // default: false
+  // `resizeTo`: Element to automatically resize stage to.
+  resizeTo: window,
+})
 
-PIXI.utils.sayHello(type)
-
-const div = document.createElement('div')
-div.innerText = 'Hello, everything is ready.'
-document.getElementById('root').appendChild(div)
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view)
